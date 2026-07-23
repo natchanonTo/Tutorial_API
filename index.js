7145a2370592e0b4a27e1b90dcfb7d110953c842
natchanonTo <natchanontomudbarung@gmail.com>
2026-07-23 18:50:13 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const router = require('./routes/myRouter');
+const path = require('path');
 const app = express();
 
-app.use(router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8080, () => {
   console.log('Server is running on http://localhost:8080');
--- routes/myRouter.js
+++ routes/myRouter.js
@@ -2,23 +2,4 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get('/', (req, res) => {
-  res.status(200);
-  res.type('text/html');
-  res.sendFile(path.join(__dirname,'../webpages/index.html'));
-});
-
-router.get('/product/:id', (req, res) => {
-  const productid = req.params.id;
-  if (productid === '1') {
-    res.sendFile(path.join(__dirname, '../webpages/product1.html'));
-  } else if (productid === '2') {
-    res.sendFile(path.join(__dirname, '../webpages/product2.html'));
-  } else if (productid === '3') {
-    res.sendFile(path.join(__dirname, '../webpages/product3.html'));
-  } else {
-    res.redirect('/');
-  }
-});
-
 module.exports = router;
